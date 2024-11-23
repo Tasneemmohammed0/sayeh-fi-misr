dedcf5397e70927c49dfaf732affcfda3ddcbeaf
Loay Ahmed <loayahmed304@gmail.com>
2024-11-23 21:23:50 +0200

    init: add initial backend code (#3)

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () =>
+  console.log(`Listening to port ${port} ...`)
+);
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandeled Rejection 💀💀 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
