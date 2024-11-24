6900d445ee3b054ba05c63dc46984d8f643d096c
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-11-24 15:52:06 +0200

    feat(nav): add navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,30 @@
+import "../styles/NavBar.css";
+import { NavLink } from "react-router-dom";
+
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <header className="navbar">
+      <div className="logo">Sayeh fe Misr</div>
+      <nav className="nav-links">
+        <ul>
+          <li>
+            <NavLink to="/" activeClassName="active-link">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/gatherings">Gatherings</NavLink>
+          </li>
+          <li>
+            <NavLink to="/trending">Trending Places</NavLink>
+          </li>
+          <li>
+            <NavLink to="/signin">Sign Up</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default NavBar;
