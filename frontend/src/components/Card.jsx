27a8d047761cd5b12c9829c150f13dc58a08970c
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-11-24 19:46:17 +0200

    feat(ui): add place card component

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { FaStar, FaRegStar } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import {
+  MdReviews,
+  MdPhotoCamera,
+  MdPeople,
+  MdOutlineArrowRightAlt,
+} from "react-icons/md";
+
+import "../styles/Card.css";
+
+function Card() {
+  const [photo, setPhoto] = useState(
+    "images/photo-1502250493741-939d1c76eaad.png"
+  );
+  const [placeName, setplaceName] = useState("Karnak Temple");
+  const [rate, setRate] = useState(2);
+  const [location, setLocation] = useState("Egypt, Cairo, Luxor");
+
+  function renderStars() {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rate) {
+        stars.push(<FaStar className="star-icon" key={i} />);
+      }
+    }
+    return stars;
+  }
+
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={photo} alt="card image" />
+        <div className="card-content">
+          <h2 className="card-title">{placeName}</h2>
+          <div className="rating">{renderStars()}</div>
+        </div>
+        <div className="card-info">
+          <div>
+            <MdReviews className="card-icon" />
+            <p>Reviews</p>
+          </div>
+          <div>
+            <MdPhotoCamera className="card-icon" />
+            <p>Photos</p>
+          </div>
+          <div>
+            <MdPeople className="card-icon" />
+            <p>People</p>
+          </div>
+        </div>
+        <div className="location">
+          <FaLocationDot className="card-icon" />
+          <p>{Location}</p>
+        </div>
+        <div className="dotted-line"></div>
+        <div className="card-footer">
+          <p>JOIN</p>
+          <MdOutlineArrowRightAlt className="right-arrow" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
