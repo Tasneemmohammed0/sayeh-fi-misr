40cc2acd10b37318e5cb46773d3496d5c6dee9f2
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-11-26 00:31:08 +0200

    fix: fixed a typo

--- database/PLACE.sql
+++ database/PLACE.sql
@@ -6,8 +6,8 @@ CREATE TABLE PLACE(
   card_photo VARCHAR,
   type VARCHAR(100),
   breif VARCHAR,
-  foreignt_adult_ticket_price INT,
-  foreignt_student_ticket_price INT,
+  foreign_adult_ticket_price INT,
+  foreign_student_ticket_price INT,
   egyptian_adult_ticket_price INT,
   egyptian_student_ticket_price INT,
   opening_hours TIME
