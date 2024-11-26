6da8d8c4d108a07f80f23d8118685fb352b38ee9
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-11-27 00:31:37 +0200

    feat(ui): add place image and the title

--- frontend/src/pages/PlaceDetails.jsx
+++ frontend/src/pages/PlaceDetails.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "../styles/placedetails.css";
 
 function PlaceDetails() {
   const { placeId } = useParams();
@@ -11,17 +12,22 @@ function PlaceDetails() {
     console.log(placeId);
     fetch(` http://localhost:1123/api/v1/places/${placeId}`)
       .then((res) => res.json())
-      .then((data) => {
-        console.log(data.data.Place);
-        return setPlace(data.data.Place);
-      })
+      .then((data) => setPlace(data.data.Place))
       .then((err) => setError(err));
   }, [placeId]);
 
   return (
     <main>
-      <div className="place-image">
+      <div
+        className="place-image"
+        style={{ backgroundImage: `url(${place.image})` }}
+      >
         <h1 className="place-title">Pyramids of Giza</h1>
+      </div>
+      <div className="place-details">
+        <div className="place-breif">
+          <h2>Breif</h2>
+        </div>
       </div>
     </main>
   );
