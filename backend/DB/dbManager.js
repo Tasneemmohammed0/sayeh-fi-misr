ed2784e0e51c9c51b86642be3e5ee8ecb8b92d6f
Loay Ahmed <loayahmed304@gmail.com>
2024-11-29 02:29:53 +0200

    feat: add some dbManager basic functions

--- backend/DB/dbManager.js
+++ backend/DB/dbManager.js
@@ -13,5 +13,36 @@ const dbConfig = {
 const db = new Pool(dbConfig);
 db.connect(() => console.log('DB Running'));
 
-module.exports = db;
+exports.ExecuteNonQuery = async (query, params = []) => {
+    try{
+        const res = await db.query(query, params);
+        return res;
+    }catch(err){
+        console.log(err.message);
+        throw err;
+    }
+}
 
+exports.ExecuteQuery = async(query, params = []) => {
+    try{
+        const res = await db.query(query, params);
+        return res.rows; // return array
+    }catch(err){
+        console.log(err.message);
+        throw err;
+    }
+}
+
+exports.ExecuteScalar = async(query, params = []) => {
+    try{
+        const res = await db.query(query, params);
+        if(res.rows.length > 0) return res.rows[0];
+        
+        return null;
+    }catch(err){
+        console.log(err.message);
+        throw err;
+    }
+}
+
+exports.close = async () => db.end();
