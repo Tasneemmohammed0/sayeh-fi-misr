e6b4462f51ec4d3c986d6f79f65cef00cb179249
Loay Ahmed <loayahmed304@gmail.com>
2024-11-29 18:01:59 +0200

    feat: display user visit list on his profile

--- frontend/src/components/VistedList.jsx
+++ frontend/src/components/VistedList.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
 import styles from "../styles/placeslist.module.css";
 import Card from "./Card";
-function VistedList({user}) {
 
+function VistedList({ user }) {
   /// fetching by user id to get the visited places
+  const { id } = useParams();
 
-  const visitedPlaces = [];
-  const temp ={
-    id:2,
-    name:"Karnak Temple",
-    city:"Luxor",
-    image:"/src/assets/images/temple.png",
-    rate:4,
-  }
-  for(let i=0;i<user.placesVisited;i++){
-    visitedPlaces.push(temp)
-  }
-
-
-
+  const [visitList, setVisitList] = React.useState([]);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:1123/api/v1/users/visitlist/${id}`
+        );
+        setVisitList(response.data.data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    fetchData();
+  }, [id]);
 
   return (
-   
     <div className={styles.list}>
-      {
-        visitedPlaces.map((item,index)=>
-        <div style={{display:"flex",justifyContent:"space-around",alignItems:"center"} }> 
-        <Card key={index} photo={item.image} placeName={item.name } location={item.city} rate={item.rate} />
-        </div>)
-        
-      }
+      {visitList.map((item, index) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Card
+            key={index}
+            photo={item.photo}
+            placeName={item.name}
+            location={item.city}
+            rate={item.rate}
+          />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default VistedList
+export default VistedList;
