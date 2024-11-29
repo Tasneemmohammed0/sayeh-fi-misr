59c5f6add65fb0188910f7ff87e5b9dceb8b83ba
Amr hany <149177595+Amrhanysayed@users.noreply.github.com>
2024-11-29 02:31:09 +0200

    feat-all gathering (#18)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import AllPlaces from './pages/AllPlaces'
 import UserProfile from './pages/UserProfile'
 import WishList from './components/WishList'
 import PlaceDetails from "./pages/PlaceDetails";
+import AllGathering from './pages/AllGathering'
 import axios from 'axios'
 function App() {
   return (
@@ -16,14 +17,16 @@ function App() {
       <Route path="/home" element={<Home />}  />
       <Route path="/signin" element={<SignIn/>} />
       <Route path="/signup" element={<SignUp/>} />
-      <Route path="/allplaces" element={<AllPlaces/>} />
+      <Route path="/places" element={<AllPlaces/>} />
+      <Route path="/gatherings" element={<AllGathering/>} />
+      
 
       <Route path="/profile" element={<UserProfile/>} />
 
       <Route path="/profile/wishlist/:id" element={<WishList/>} />
 
       <Route path="/places/:placeId" element={<PlaceDetails />} />
-      <Route path="/profile" element={<UserProfile/>} />
+      {/* <Route path="/profile" element={<UserProfile/>} /> */}
 
     </Routes>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import TrendingPlaces from '../components/TrendingPlaces'
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
+import Loading from '../components/Loading';
 
 function Home() {
 
@@ -30,6 +31,9 @@ function Home() {
       <NavBar />
       <Hero />
       <TrendingPlaces places={trendingPlaces} />
+      {/*for testing only any one can change color for more matching colors */}
+      {/* <Loading /> */}  
+      
     </main>
   );
 }
