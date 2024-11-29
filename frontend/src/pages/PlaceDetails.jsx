f4da6e86cee65dc76000c805d886967fb532da66
amirakhaled928 <amirakhaled928@gmail.com>
2024-11-29 23:26:14 +0200

    Feat: add place details

--- frontend/src/pages/PlaceDetails.jsx
+++ frontend/src/pages/PlaceDetails.jsx
@@ -1,89 +1,49 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import styles from "../styles/PlaceDetails.module.css";
+import styles from "../styles/placeDetails.module.css";
 import SeeMoreText from "../components/SeeMoreText";
 import PlaceTicketPrice from "../components/PlaceTicketPrice";
-import OpeningHours from "../components/OpeningHours";
-import PlaceLocation from "../components/PlaceLocation";
-import ReviewCards from "../components/ReviewCards";
-import { IoAddCircleSharp } from "react-icons/io5";
-import ReviewForm from "../components/ReviewForm";
-
+import axios from "axios";
 function PlaceDetails() {
   const { placeId } = useParams();
   const [place, setPlace] = useState({});
   const [error, setError] = useState(null);
-  const [reviews, setReviews] = useState([]);
-  const [isReviewFormOpen, setIsReviewFormOpen] = useState(false);
-
-
-  // fetch place data from the API
+  console.log(placeId);
   useEffect(() => {
-    console.log(placeId);
-    fetch(` http://localhost:1123/api/v1/places/${placeId}`)
-      .then((res) => res.json())
-      .then((data) => setPlace(data.data.Place))
-      .then((err) => setError(err));
-  }, [placeId]);
-  // review => {rating, title, main_content, date, user_id, place_id, username, profile_photo}
-  // fetch place reviews
-  useState(() => {
-    fetch(` http://localhost:1123/api/v1/reviews/${placeId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.data.reviews);
-        setReviews(data.data.reviews);
-      });
-  }, [placeId]);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:1123/api/v1/places/${placeId}`
+        );
+        console.log(response.data.data);
+        if (response.status === "fail") {
+          console.log("error");
+          return;
+        }
+        setPlace(response.data.data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <main className={styles.main}>
+    <main className={styles.mainDetailsPlace}>
       <div
         className={styles.backgroundImage}
-        style={{ backgroundImage: `url(${place.image})` }}
+        style={{ backgroundImage: `url(${place.photo})` }}
       >
         <h1 className={styles.title}>Pyramids of Giza</h1>
       </div>
       <div className={styles.container}>
         <div className={styles.breif}>
-          <h3>Breif</h3>
+          <h3 style={{ fontSize: "1.6rem", color: "#0c3e69" }}>Breif</h3>
           <SeeMoreText className={styles.breifText} text={place.breif} />
         </div>
         <hr></hr>
-        <div className={styles.info}>
-          <PlaceTicketPrice
-            egyptianChildPrice={place.egyptian_child_ticket_price}
-            egyptianAdultPrice={place.egyptian_adult_ticket_price}
-            otherChildPrice={place.foreign_child_ticket_price}
-            otherAdultPrice={place.foreign_adult_ticket_price}
-          />
-          {/* <hr className={styles.vLine}></hr> */}
-          <div>
-            <OpeningHours
-              openingHoursNormal={place.opening_hours_normal}
-              openingHoursHoliday={place.opening_hours_holidays}
-            />
-            <PlaceLocation location={place.location} />
-          </div>
-        </div>
-        <hr></hr>
-        <div className={styles.reviewSection}>
-          <div className={styles.reviewsHeader}>
-            <h3>See what visitors are saying</h3>
-            <div style={{ display: "flex", gap: "0.6rem" }}>
-              <h5>Share Your Review</h5>
-
-              <IoAddCircleSharp
-                className={styles.addIcon}
-                onClick={() => setIsReviewFormOpen(!isReviewFormOpen)}
-              />
-            </div>
-          </div>
-          <ReviewCards reviews={reviews} />
-          <ReviewForm
-            isOpen={isReviewFormOpen}
-            setIsOpen={setIsReviewFormOpen}
-          />
+        <div>
+          <PlaceTicketPrice />
         </div>
       </div>
     </main>
