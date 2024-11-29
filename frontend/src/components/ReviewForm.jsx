f298aa002252b073fe75006bc6b11570fdcce924
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-11-29 16:54:41 +0200

    feat: add the review rate functionality

--- frontend/src/components/ReviewForm.jsx
+++ frontend/src/components/ReviewForm.jsx
@@ -1,10 +1,12 @@
 import styles from "../styles/reviewForm.module.css";
 import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import Rate from "./Rate";
 
 function ReviewForm({ isOpen, setIsOpen }) {
   const [title, setTitle] = useState("");
   const [review, setReview] = useState("");
+  const [rate, setRate] = useState(0);
   const [error, setError] = useState(false);
 
   if (!isOpen) return null;
@@ -12,9 +14,12 @@ function ReviewForm({ isOpen, setIsOpen }) {
   function handleSubmit(e) {
     // sent the review to the API
     e.preventDefault();
-    console.log(`Title: ${title}`);
 
-    console.log(`Review: ${review}`);
+    if (!rate) {
+      setError(true);
+      alert("⭐ Please rate the place");
+      return;
+    }
 
     // show success message
     alert("🎉 Review Submitted Successfully!");
@@ -23,15 +28,25 @@ function ReviewForm({ isOpen, setIsOpen }) {
     setTitle("");
     setReview("");
     setIsOpen(false);
+    setRate(0);
+  }
+
+  function handleClose() {
+    // clear the form
+    setTitle("");
+    setReview("");
+    setIsOpen(false);
+    setRate(0);
   }
 
   return (
     <div className={styles.popupOverlay}>
       <form className={styles.form} onSubmit={handleSubmit}>
         <h3>Share your experience with us</h3>
-        <button className={styles.popupClose} onClick={() => setIsOpen(false)}>
+        <button className={styles.popupClose} onClick={handleClose}>
           <IoMdClose />
         </button>
+        <Rate rate={rate} setRate={setRate} />
         <label className={styles.formLabel}>
           Title
           <input
