dc0ab4d119eca08d7f2a53d522f359308613767d
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-11-30 16:55:10 +0200

    refactor: delete review router and controller

--- backend/routes/reviewRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const reviewController = require("../controllers/reviewController");
-const router = express.Router();
-
-router.get("/:placeID", reviewController.getAllReviews); //http://localhost:1123/api/v1/reviews/:placeId
-
-module.exports = router;
