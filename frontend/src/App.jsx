74421a9139391ea4756c22eecd914864dff196a1
Amr hany <149177595+Amrhanysayed@users.noreply.github.com>
2024-12-02 01:03:39 +0200

    feat-usersetting without good styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,22 +8,25 @@ import UserProfile from "./pages/UserProfile";
 import PlaceDetails from "./pages/PlaceDetails";
 import WishList from "./components/WishList";
 import AllGathering from "./pages/AllGathering";
+import AccountSetting from "./pages/AccountSetting";
 import axios from "axios";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} index />
       <Route path="/home" element={<Home />} />
-        
+
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
 
       <Route path="/places/:placeId" element={<PlaceDetails />} />
       <Route path="/places" element={<AllPlaces />} />
-        
+
       <Route path="/gatherings" element={<AllGathering />} />
-        
+
       <Route path="/profile/:id" element={<UserProfile />} />
+      <Route path="/profile/:id/accountsetting" element={<AccountSetting />} />
+
       <Route path="/profile" element={<UserProfile />} />
       <Route path="/profile/wishlist/:id" element={<WishList />} />
       <Route path="/places/:placeId" element={<PlaceDetails />} />
