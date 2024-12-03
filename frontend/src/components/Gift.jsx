0ad1ca8d9604bdaee0b73db40cf5d1c4bf02da43
Loay Ahmed <loayahmed304@gmail.com>
2024-12-03 14:58:00 +0200

    feat: adjust gift card props

--- frontend/src/components/Gift.jsx
+++ frontend/src/components/Gift.jsx
@@ -1,18 +1,23 @@
 import styles from "../styles/Gift.module.css";
 import { BiCoinStack } from "react-icons/bi";
 
-function Gift({ photo = "../src/assets/images/gift-1.jpg" }) {
+function Gift({
+  photo = "../src/assets/images/gift-1.jpg",
+  price = "198",
+  title = "Something Earring",
+  place = "Pyramids of Giza",
+}) {
   return (
     <main className={styles.mainContainer}>
       <img src={photo} alt="gift 1" className={styles.giftImage}></img>
       <div className={styles.giftDetailsContainer}>
         <div className={styles.priceContainer}>
           <BiCoinStack color="orange" size="1.3rem" />
-          <p className={styles.priceText}>198</p>
+          <p className={styles.priceText}>{price}</p>
         </div>
         <div className={styles.descriptionContainer}>
-          <div className={styles.giftTitle}>Something Earring</div>
-          <div className={styles.giftPlace}>Pyramids of Giza</div>
+          <div className={styles.giftTitle}>{title}</div>
+          <div className={styles.giftPlace}>{place}</div>
         </div>
       </div>
     </main>
