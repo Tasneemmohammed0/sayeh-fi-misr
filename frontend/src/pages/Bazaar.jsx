2400013b4a85cf4524b7cfe28ae9f0d64edda8f3
Loay Ahmed <loayahmed304@gmail.com>
2024-12-03 14:55:57 +0200

    feat: finalize gifts container styles

--- frontend/src/pages/Bazaar.jsx
+++ frontend/src/pages/Bazaar.jsx
@@ -2,6 +2,11 @@ import styles from "../styles/bazaar.module.css";
 import Gift from "../components/Gift";
 
 function Bazaar() {
+  const gifts = [];
+  for (let i = 0; i < 7; i += 1) {
+    gifts.push(<Gift />);
+  }
+
   return (
     <main className={styles.mainBazaar}>
       <div className={styles.bazaarHeader}>
@@ -9,12 +14,7 @@ function Bazaar() {
         <p>Spend your points and get prizes for free</p>
       </div>
       <div className={styles.bazaarContent}>
-        <div>
-          <Gift />
-          {/* <Gift />
-          <Gift />
-          <Gift /> */}
-        </div>
+        <div className={styles.giftContainer}>{gifts.map((gift) => gift)}</div>
       </div>
     </main>
   );
