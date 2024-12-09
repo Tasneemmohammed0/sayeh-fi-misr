309df99f1c94e59668fef347264ca58541618ccc
Amira Khalid <149877108+AmiraKhalid04@users.noreply.github.com>
2024-12-09 21:41:16 +0200

    fix: edit get gathering query in explore section (#62)

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -18,9 +18,20 @@ exports.getExplorePlaces = async (req, res) => {
 
 exports.getExploreGatherings = async (req, res) => {
   try {
-    const data = await db.query(`SELECT g.*, p.photo,p.city
-FROM gathering g
-JOIN place p ON g.place_id = p.place_id
+    const data = await db.query(`SELECT 
+    g.*,
+    p.photo,
+    p.city,
+    h.first_name,
+    (SELECT COUNT(*) 
+     FROM visitor_gathering vg 
+     WHERE vg.gathering_id = g.gathering_id) AS current_capacity
+FROM 
+    gathering g
+JOIN 
+    place p ON g.place_id = p.place_id
+JOIN 
+    host h ON g.host_id = h.user_id
 ORDER BY gathering_id ASC LIMIT 4
 `);
 
