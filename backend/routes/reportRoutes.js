2511c267798d1b69b38de5e58699ce1dd96e64f6
Loay Ahmed <loayahmed304@gmail.com>
2024-12-11 18:30:44 +0200

    refactor: change post report endpoint route for consistency

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -7,7 +7,7 @@ const reportController = require("../controllers/reportController");
 // All routes are restricted to only logged in users
 router.use(authController.protect);
 
-router.post("/add", reportController.addReport);
+router.post("/", reportController.addReport);
 
 // Starting from here, routes are restricted to only admins
 router.use(authController.restrictTo("admin"));
