0683da0ef3413d4ac033574eb0c57569f26c4b1c
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-12-12 19:10:03 +0200

    fix: if no review is deleted send 400 response

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,11 +1,22 @@
+const db = require("../db/index");
+
 // Delete a review
 exports.deleteReview = async (req, res) => {
   try {
+    console.log("In delete review");
+    console.log(req.user.user_id);
     await db.query("COMMIT");
     const data = await db.query(
       `DELETE FROM review WHERE review_id=$1 AND user_id=$2 RETURNING *`,
       [req.params.id, req.user.user_id]
     );
+
+    if (!data.rowCount) {
+      res.status(400).json({
+        message: "Failed to delete",
+      });
+      return;
+    }
 
     res.status(200).json({
       status: "success",
