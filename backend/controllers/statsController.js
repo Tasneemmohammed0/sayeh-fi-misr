7ef5fa2b0f6ecf1f9db6846a6adc69383bbfb0f0
Loay Ahmed <loayahmed304@gmail.com>
2024-12-18 01:10:29 +0200

    feat(backend): add total visitors and their types endpoint (#96)

--- backend/controllers/statsController.js
+++ backend/controllers/statsController.js
@@ -1,1 +1,28 @@
 const db = require("../db/index");
+
+exports.getUsersTypes = async (req, res, next) => {
+  try {
+    const query = `
+      SELECT role, COUNT(DISTINCT user_id) AS count
+      FROM visitor
+      GROUP BY (role)
+      `;
+    const response = await db.query(query);
+    if (!response.rowCount) {
+      return res.status(404).json({
+        status: "fail",
+        message: "No users found",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      length: response.rowCount,
+      data: response.rows,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
