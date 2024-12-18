8ff09222fd7720853fea37505d9341c65afdf04f
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-12-18 02:08:28 +0200

    feat(backend): add top 5 nationalities endpoint

--- backend/controllers/statsController.js
+++ backend/controllers/statsController.js
@@ -1,1 +1,22 @@
 const db = require("../db/index");
+
+// Get top 5 nationalities
+exports.getTopFiveNationalities = async (req, res) => {
+  try {
+    const data = await db.query(`SELECT nationality, COUNT(*) AS user_count
+    FROM visitor_nationality GROUP BY nationality
+    ORDER BY user_count DESC LIMIT 5`);
+
+    res.status(200).json({
+      status: "success",
+      legnth: data.rows.length,
+      data: data.rows,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+    console.error(err);
+  }
+};
--- backend/routes/statsRoutes.js
+++ backend/routes/statsRoutes.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const statsController = require("../controllers/statsController");
+const authController = require("../controllers/authController");
 
 module.exports = router;
+
+// Top 5 nationalities
+router.get(
+  "/nationalities",
+  authController.protect,
+  authController.restrictTo("admin"),
+  statsController.getTopFiveNationalities
+);
